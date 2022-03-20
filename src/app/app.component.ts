cd41ec8ca600dbefcd7aad3988a56559bbe86a36
Oscar Mateu Giner <OMateuGiner@lear.com>
2022-03-20 11:24:37 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { SummaryResolver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector:'app-root',
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  title = 'bases';
+ 
 }
+
